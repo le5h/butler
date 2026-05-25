5f70ae825c2b3f19dc8b2a0fe04845c4c5d3a2c3
Alexei Gaiduk <le5h@yandex.ru>
2026-05-25 09:33:47 +0300

    refactor: add api() helper to tracker snippet
    Share fetch/JSON.stringify/Content-Type boilerplate between
    api=new call. Update path stays on sendBeacon (different API).
    
    index.php | 3 ++-
     1 file changed, 2 insertions(+), 1 deletion(-)

--- index.php
+++ index.php
@@ -48,7 +48,8 @@ let data={};
 if(collect.referrer)data.referrer=document.referrer;
 if(collect.lang)data.lang=navigator.language;
 if(collect.page)data.page=location.pathname;
-fetch(base+'?api=new',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify(data)}).then(function(r){return r.json()}).then(function(d){id=d.id})['catch'](function(){});
+function api(m,d){return fetch(base+'?api='+m,{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify(d)}).then(function(r){return r.json()})}
+api('new',data).then(function(d){id=d.id})['catch'](function(){});
 function send(){if(!id||send.s)return;send.s=1;let sec=((Date.now()-start)/1e3).toFixed(1);try{(navigator.sendBeacon||function(url,body){fetch(url,{method:'POST',body:body,keepalive:!0})})(base+'?api=update',JSON.stringify({id:id,duration:sec,interactions:ints}))}catch(e){}}
 document.addEventListener('visibilitychange',function(){document.visibilityState==='hidden'?send():send.s=0});
 window.addEventListener('beforeunload',send);})();
